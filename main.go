bbd9885c84154e2322eb0b509e16eb74cea8f729
Julien Del-Piccolo <julien@del-piccolo.com>
2017-11-16 00:58:08 +0100

    gops agent

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"log"
+
+	"github.com/google/gops/agent"
 	"github.com/jdel/acdc/cmd"
 )
 
 func main() {
+	if err := agent.Listen(agent.Options{}); err != nil {
+		log.Fatal(err)
+	}
 	cmd.Execute()
 }
